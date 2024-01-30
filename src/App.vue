ce2da550b5a3079a87403d6fa62dae01477fd5cf
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-01-30 19:40:47 +0900

    lecture 14

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
+    <!-- <div v-show="toggle">true</div>
+    <div v-show="!toggle">false</div>
+    <div v-if="toggle">true</div>
+    <div v-else>false</div>
+    <button @click="onToggle">Toggle</button> -->
     <div class="container">
         <h2 class="mt-2">To-Do List</h2>
-        <form class="d-flex" @submit.prevent="onSubmit">
-            <div class="flex-grow-1 me-2">
-                <input class="form-control" type="text" v-model="todo" placeholder="Type new to-do" />
+        <form @submit.prevent="onSubmit">
+            <div class="d-flex">
+                <div class="flex-grow-1 me-2">
+                    <input class="form-control" type="text" v-model="todo" placeholder="Type new to-do" />
+                </div>
+                <div>
+                    <button class="btn btn-primary" type="submit">Add</button>
+                </div>
             </div>
-            <div>
-                <button class="btn btn-primary" type="submit">Add</button>
-            </div>
+
+            <div v-show="hasError" style="color: red">This field cannot be empty</div>
         </form>
         <div v-for="todo in todos" :key="todo.id" class="card mt-2">
             <div class="card-body p-2">
@@ -22,22 +31,36 @@ import { ref } from "vue";
 
 export default {
     setup() {
+        // const toggle = ref(false);
         const todo = ref("");
         const todos = ref([
             { id: 1, subject: "buy apple" },
             { id: 2, subject: "buy banana" },
         ]);
 
+        const hasError = ref(false);
+
         const onSubmit = () => {
-            todos.value.push({
-                id: Date.now(),
-                subject: todo.value,
-            });
+            if (todo.value === "") {
+                hasError.value = true;
+            } else {
+                hasError.value = false;
+                todos.value.push({
+                    id: Date.now(),
+                    subject: todo.value,
+                });
+            }
         };
+
+        // const onToggle = () => {
+        //     toggle.value = !toggle.value;
+        // };
 
         return {
             todo,
             todos,
+            // toggle,
+            hasError,
             onSubmit,
         };
     },
