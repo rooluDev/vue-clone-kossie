4a7c5b1cd752ce8e4b7684cecbe679067b35837d
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-01-30 19:22:03 +0900

    lecture 12

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,24 @@
     <div class="container">
         <h2>To-Do List</h2>
         <form class="d-flex" @submit.prevent="onSubmit">
-            <div class="flex-grow-1 mr-2">
+            <div class="flex-grow-1 m-2">
                 <input class="form-control" type="text" v-model="todo" placeholder="Type new to-do" />
             </div>
             <div>
                 <button class="btn btn-primary" type="submit">Add</button>
             </div>
         </form>
-        {{ todos }}
+
+        <div class="card mt-2">
+            <div class="card-body p-2">
+                {{ todos[0].subject }}
+            </div>
+        </div>
+        <div class="card mt-2">
+            <div class="card-body p-2">
+                {{ todos[1].subject }}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -19,7 +29,10 @@ import { ref } from "vue";
 export default {
     setup() {
         const todo = ref("");
-        const todos = ref([]);
+        const todos = ref([
+            { id: 1, subject: "buy apple" },
+            { id: 2, subject: "buy banana" },
+        ]);
 
         const onSubmit = () => {
             todos.value.push({
