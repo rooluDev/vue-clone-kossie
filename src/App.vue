7ccefc1cd99d6d33ddca380f572492d31c60317a
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-11 22:32:36 +0900

    lecture 35

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,14 @@
         <nav aria-label="Page navigation example">
             <ul class="pagination">
                 <li v-if="currentPage !== 1" class="page-item">
-                    <a class="page-link" href="#">Previous</a>
+                    <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage - 1)">Previous</a>
                 </li>
                 <li v-for="page in numberOfPages" :key="page" class="page-item"
                     :class="currentPage === page ? 'active' : ''">
-                    <a class="page-link" href="#">{{ page }}</a>
+                    <a style="cursor: pointer" class="page-link" @click="getTodos(page)">{{ page }}</a>
                 </li>
                 <li v-if="numberOfPages !== currentPage" class="page-item">
-                    <a class="page-link" href="#">Next</a>
+                    <a style="cursor: pointer" class="page-link" @click="getTodos(currentPage + 1)">Next</a>
                 </li>
             </ul>
         </nav>
@@ -50,10 +50,11 @@ export default {
         })
 
 
-        const getTodos = async () => {
+        const getTodos = async (page = currentPage.value) => {
+            currentPage.value = page;
             try {
                 const res = await axios.get(
-                    `http://localhost:3000/todos?_page=${currentPage.value}&_limit=${limit}`
+                    `http://localhost:3000/todos?_page=${page}&_limit=${limit}`
                 );
                 numberOfTodos.value = res.headers['x-total-count'];
                 todos.value = res.data;
@@ -130,7 +131,8 @@ export default {
             toggleTodo,
             filtreredTodos,
             numberOfPages,
-            currentPage
+            currentPage,
+            getTodos
         };
     }
 };
