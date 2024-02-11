847b10e6d7da6822625e2fce74b82c7a98755abd
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-12 00:39:00 +0900

    lecture 44

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Roolu Dev</a>
+            <router-link class="navbar-brand" to="/">Roolu Dev</router-link>
 
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Todos</a>
+                    <router-link class="nav-link active" aria-current="page" to="/todos">Todos</router-link>
                 </li>
             </ul>
         </div>
