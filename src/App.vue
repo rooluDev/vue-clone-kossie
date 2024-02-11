f000ae5407ded114adc48588730ef82f2b24f776
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-12 00:35:23 +0900

    lecture 43

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Roolu Dev</a>
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Todos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <router-view />
 </template>
 
