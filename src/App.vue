0983db35df63869d1875dabb5e95c05b313c01a8
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-11 16:00:47 +0900

    lecture 27

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
         <input class="form-control" type="text" v-model="searchText" placeholder="Search" />
         <hr />
         <TodoSimpleForm @add-todo="addTodo" />
+        <div style="color: red">{{ error }}</div>
         <div v-if="!filtreredTodos.length">There is nothig to display</div>
         <TodoList :todos="filtreredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" />
     </div>
@@ -13,6 +14,7 @@
 import { ref, computed } from "vue";
 import TodoSimpleForm from "./components/TodoSimpleForm.vue";
 import TodoList from "./components/TodoList.vue";
+import axios from "axios";
 
 export default {
     components: {
@@ -22,13 +24,25 @@ export default {
 
     setup() {
         const todos = ref([]);
+        const error = ref("");
+
         const todoStyle = {
             textDecoration: "line-through",
             color: "gray",
         };
 
         const addTodo = (todo) => {
-            todos.value.push(todo);
+            error.value = "";
+            axios
+                .post("http://localhost:3000/todos", { subject: todo.subject, completed: todo.completed })
+                .then((res) => {
+                    console.log(res);
+                    todos.value.push(res.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    error.value = "Something went wrong.";
+                });
         };
 
         const toggleTodo = (index) => {
@@ -53,6 +67,7 @@ export default {
 
         return {
             todos,
+            error,
             todoStyle,
             searchText,
             addTodo,
