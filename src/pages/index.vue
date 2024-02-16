3b7631fe97aa16d2eed2e2695ebdcd94a58dc9d1
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-17 01:17:31 +0900

    lecture 73

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,19 @@
 <template>
     <div>Home Page</div>
+    <div>{{ count }}</div>
+    <button @click="count++">Add</button>
 </template>
 
 <script>
+import { useCount } from '@/composables/count';
 export default {
+    setup() {
+        const { count } = useCount();
 
+        return {
+            count
+        }
+    }
 }
 </script>
 
